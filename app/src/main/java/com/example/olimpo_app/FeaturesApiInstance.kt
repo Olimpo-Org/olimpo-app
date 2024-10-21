659d451be337690faf6ef27d6929c926d0f3ab1b
RaphaCosil <Raphael.Cosil@gmail.com>
2024-10-21 13:08:53 -0300

    Fix - corrigindo criação do Retrofit

--- app/src/main/java/com/example/olimpo_app/FeaturesApiInstance.kt
+++ app/src/main/java/com/example/olimpo_app/FeaturesApiInstance.kt
@@ -1,2 +1,17 @@
 package com.example.olimpo_app
 
+import com.example.olimpo_app.data.network.FeaturesAPIService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object FeaturesApiInstance {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+    val api: FeaturesAPIService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(FeaturesAPIService::class.java)
+    }
+}
