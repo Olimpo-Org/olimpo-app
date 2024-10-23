c1e98ce0f2eb9de7071f67ec2da605b64d941989
RaphaCosil <Raphael.Cosil@gmail.com>
2024-10-23 12:39:34 -0300

    ADD - adicionando fluxo da api, não terminado

--- app/src/main/java/com/example/olimpo_app/presentation/activity/accessFlow/CadastroActivity.kt
+++ app/src/main/java/com/example/olimpo_app/presentation/activity/accessFlow/CadastroActivity.kt
@@ -12,11 +12,16 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.olimpo_app.AccessApiInstance
 import com.example.olimpo_app.R
+import com.example.olimpo_app.data.model.accessFlow.UserAPI
+import com.example.olimpo_app.data.repository.UserRepository
 import com.example.olimpo_app.databinding.ActivityCadastroBinding
 import com.example.olimpo_app.utils.Constants
 import com.example.olimpo_app.utils.PreferenceManager
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
 import java.io.ByteArrayOutputStream
 import java.io.FileNotFoundException
 
@@ -24,6 +29,8 @@ class CadastroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCadastroBinding
     private lateinit var preferenceManager: PreferenceManager
     private var encodedImage: String? = null
+    private val accessRepository = UserRepository(AccessApiInstance.service)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCadastroBinding.inflate(layoutInflater)
@@ -31,26 +38,32 @@ class CadastroActivity : AppCompatActivity() {
         setContentView(binding.root)
         setListeners()
     }
+
     private fun setListeners(){
         binding.textLogin.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
         binding.buttonSignIn.setOnClickListener{
             if (isValidSignUpDetails()){
-                signUp()
+                signUpFirebaseAndApi() // Chama os dois métodos de cadastro
             }
         }
         binding.fotoPerfil.setOnClickListener {
-            val galeriaIntent =
-                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            val galeriaIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             galeriaIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
             pickImage.launch(galeriaIntent)
         }
     }
+
     private fun showToast(message: String){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
     }
 
-    private fun signUp() {
+    private fun signUpFirebaseAndApi() {
+        signUpFirebase {
+            singUpApi()
+        }
+    }
+
+    private fun signUpFirebase(onSuccess: () -> Unit) {
         loading(true)
         val database = FirebaseFirestore.getInstance()
         val user = hashMapOf(
@@ -67,16 +80,46 @@ class CadastroActivity : AppCompatActivity() {
                 preferenceManager.putString(Constants.KEY_USER_ID, it.id)
                 preferenceManager.putString(Constants.KEY_NAME, binding.inputName.text.toString())
                 preferenceManager.putString(Constants.KEY_IMAGE, encodedImage!!)
-                val intent = Intent(applicationContext, MainActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                startActivity(intent)
+                onSuccess()
             }
             .addOnFailureListener { e ->
                 loading(false)
                 e.message?.let { message -> showToast(message) }
             }
     }
-    private fun encodeImage(bitmap: Bitmap): String{
+
+    private fun singUpApi() {
+        lifecycleScope.launch {
+            val user = UserAPI(
+                null,
+                binding.inputEmail.text.toString(),
+                binding.inputPassword.text.toString(),
+                binding.inputName.text.toString(),
+                binding.inputSurname?.text.toString(),
+                binding.inputCpf?.text.toString(),
+                encodedImage!!,
+                if(binding.genero?.text == "masculino") 1 else 2,
+            )
+            val response = accessRepository.createUser(user)
+            response.toString()
+            if (response.isSuccessful && response.body() != null) {
+                response.body()!!.id?.let {
+                    preferenceManager.putLong(Constants.KEY_USER_ID, it)
+                }
+                preferenceManager.putString(Constants.KEY_NAME, response.body()!!.name)
+                preferenceManager.putString(Constants.KEY_IMAGE, response.body()!!.profileImage)
+                preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true)
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
+            } else {
+                loading(false)
+                showToast("Falha ao cadastrar via API")
+            }
+        }
+    }
+
+    private fun encodeImage(bitmap: Bitmap): String {
         val previewWidth = 150
         val previewHeight = bitmap.height * previewWidth / bitmap.width
         val previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false)
@@ -87,8 +130,7 @@ class CadastroActivity : AppCompatActivity() {
     }
 
     private val pickImage: ActivityResultLauncher<Intent> = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult())
-    {
+        ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == RESULT_OK) {
             if (it.data != null) {
                 val imageUri = it.data?.data
@@ -105,74 +147,36 @@ class CadastroActivity : AppCompatActivity() {
     }
 
     private fun isValidSignUpDetails(): Boolean {
-        if(binding.inputName.text.toString().trim().isEmpty() and binding.inputEmail.text.toString().trim().isEmpty() and binding.inputPassword.text.toString().trim().isEmpty() and binding.confirmPassword.text.toString().trim().isEmpty()){
+        if(binding.inputName.text.toString().trim().isEmpty() ||
+            binding.inputEmail.text.toString().trim().isEmpty() ||
+            binding.inputPassword.text.toString().trim().isEmpty()) {
             binding.textNome.setTextColor(getColor(R.color.Red_Bad))
             binding.textEmail.setTextColor(getColor(R.color.Red_Bad))
             binding.textSenha.setTextColor(getColor(R.color.Red_Bad))
-            binding.textConfirmSenha.setTextColor(getColor(R.color.Red_Bad))
             binding.errorMessage.visibility = View.VISIBLE
             return false
-        }else if(encodedImage == null){
+        } else if (encodedImage == null) {
             binding.errorMessage.visibility = View.VISIBLE
             showToast("Insira uma foto de perfil")
             return false
-        }else if(binding.inputName.text.toString().trim().isEmpty()) {
-            binding.textNome.setTextColor(getColor(R.color.Red_Bad))
-            binding.textEmail.setTextColor(getColor(R.color.Blue))
-            binding.textSenha.setTextColor(getColor(R.color.Blue))
-            binding.textConfirmSenha.setTextColor(getColor(R.color.Blue))
-            binding.errorMessage.visibility = View.VISIBLE
-            return false
-        }else if(binding.inputEmail.text.toString().trim().isEmpty()){
-            binding.textNome.setTextColor(getColor(R.color.Blue))
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.text.toString()).matches()) {
             binding.textEmail.setTextColor(getColor(R.color.Red_Bad))
-            binding.textSenha.setTextColor(getColor(R.color.Blue))
-            binding.textConfirmSenha.setTextColor(getColor(R.color.Blue))
             binding.errorMessage.visibility = View.VISIBLE
             return false
-        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.text.toString()).matches()){
-            binding.textNome.setTextColor(getColor(R.color.Blue))
-            binding.textEmail.setTextColor(getColor(R.color.Red_Bad))
-            binding.textSenha.setTextColor(getColor(R.color.Blue))
-            binding.textConfirmSenha.setTextColor(getColor(R.color.Blue))
-            binding.errorMessage.visibility = View.VISIBLE
-            return false
-        }else if(binding.inputPassword.text.toString().trim().isEmpty()){
-            binding.textNome.setTextColor(getColor(R.color.Blue))
-            binding.textEmail.setTextColor(getColor(R.color.Blue))
-            binding.textSenha.setTextColor(getColor(R.color.Red_Bad))
-            binding.textConfirmSenha.setTextColor(getColor(R.color.Blue))
-            binding.errorMessage.visibility = View.VISIBLE
-            return false
-        }else if(binding.confirmPassword.text.toString().trim().isEmpty()){
+        } else {
             binding.textNome.setTextColor(getColor(R.color.Blue))
             binding.textEmail.setTextColor(getColor(R.color.Blue))
             binding.textSenha.setTextColor(getColor(R.color.Blue))
-            binding.textConfirmSenha.setTextColor(getColor(R.color.Red_Bad))
-            binding.errorMessage.visibility = View.VISIBLE
-            return false
-        }else if(binding.inputPassword.text.toString() != binding.confirmPassword.text.toString()){
-            binding.textNome.setTextColor(getColor(R.color.Blue))
-            binding.textEmail.setTextColor(getColor(R.color.Blue))
-            binding.textSenha.setTextColor(getColor(R.color.Red_Bad))
-            binding.textConfirmSenha.setTextColor(getColor(R.color.Red_Bad))
-            binding.errorMessage.visibility = View.VISIBLE
-            return false
-        }else{
-            binding.textSenha.setTextColor(getColor(R.color.Blue))
-            binding.textNome.setTextColor(getColor(R.color.Blue))
-            binding.textEmail.setTextColor(getColor(R.color.Blue))
-            binding.textSenha.setTextColor(getColor(R.color.Blue))
-            binding.textConfirmSenha.setTextColor(getColor(R.color.Blue))
             binding.errorMessage.visibility = View.GONE
             return true
         }
     }
+
     private fun loading(isLoading: Boolean){
         if(isLoading){
             binding.buttonSignIn.visibility = View.INVISIBLE
             binding.progressBar.visibility = View.VISIBLE
-        }else{
+        } else {
             binding.progressBar.visibility = View.INVISIBLE
             binding.buttonSignIn.visibility = View.VISIBLE
         }
--- app/src/main/java/com/example/olimpo_app/presentation/activity/accessFlow/SolicitacaoActivity.kt
+++ app/src/main/java/com/example/olimpo_app/presentation/activity/accessFlow/SolicitacaoActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.example.olimpo_app.AccessApiInstance
 import com.example.olimpo_app.data.model.accessFlow.User
+import com.example.olimpo_app.data.repository.CommunityRepository
 import com.example.olimpo_app.databinding.ActivitySolicitacaoBinding
 import com.example.olimpo_app.presentation.activity.BaseActivity
 import com.example.olimpo_app.presentation.listeners.UserListener
@@ -13,19 +16,22 @@ import com.example.olimpo_app.utils.Constants
 import com.example.olimpo_app.utils.PreferenceManager
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
 
 class SolicitacaoActivity : BaseActivity(), UserListener {
     private lateinit var binding: ActivitySolicitacaoBinding
     private lateinit var preferenceManager: PreferenceManager
+    private val communityRepository = CommunityRepository(
+        AccessApiInstance.service
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySolicitacaoBinding.inflate(layoutInflater)
         preferenceManager = PreferenceManager(applicationContext)
         setContentView(binding.root)
 
-
         setListeners()
-        getUsers()
+        getUsersFirebase()
 
         binding.buttonArrow.setOnClickListener {
             val intent = Intent(applicationContext, MainActivity::class.java)
@@ -38,7 +44,7 @@ class SolicitacaoActivity : BaseActivity(), UserListener {
             finish()
         }
     }
-    private fun getUsers(){
+    private fun getUsersFirebase(){
         val database = FirebaseFirestore.getInstance()
         database.collection(Constants.KEY_COLLECTION_USERS)
             .get()
@@ -63,14 +69,34 @@ class SolicitacaoActivity : BaseActivity(), UserListener {
                         val acceptUsersAdapter = AcceptUsersAdapter(users, this)
                         binding.UsersRecyclerView.adapter = acceptUsersAdapter
                         binding.UsersRecyclerView.visibility = View.VISIBLE
-                    }else{
-
                     }
-                }else{
-
                 }
             }
     }
+
+
+    private fun getUsersApi(){
+        try {
+            lifecycleScope.launch {
+                val response = communityRepository.getAllSolicitations(
+                    preferenceManager.getString(Constants.KEY_COMMUNITY_ID)!!.toLong()
+                )
+
+                if (response.isSuccessful && response.body() != null){
+                    val users = response.body()!!
+                    if (users.isNotEmpty()){
+                        val acceptUsersAdapter = AcceptUsersAdapter(users, this@SolicitacaoActivity)
+                        binding.UsersRecyclerView.adapter = acceptUsersAdapter
+                        binding.UsersRecyclerView.visibility = View.VISIBLE
+                    }
+                }
+            }
+
+        } catch (e: Exception) {
+
+        }
+    }
+
     private fun setListeners(){
         binding.buttonSignOut.setOnClickListener { signOut() }
     }
